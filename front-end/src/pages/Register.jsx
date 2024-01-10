5520e6469eac33ba23e72acfa2e65234e93ed2a5
Hendra <hendra.i21@mhs.istts.ac.id>
2024-01-10 13:32:19 +0700

    fix register

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,175 +1,202 @@
-import { NavLink, Outlet } from "react-router-dom";
-import React, { useState } from 'react';
-import { TextField, Button, Container, Typography, Paper } from '@mui/material';
-import bgRegister from '../assets/bgRegister.svg';
-import bgRegDog from '../assets/bgRegDog.svg';
-import bgRegCat from '../assets/bgRegCat.svg';
-import bgRegKiri from '../assets/bgRegKiri.svg';
-import Navbar from "../components/Navbar";
-import {useAuth} from "../components/Auth"
+import { NavLink, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import {
+	TextField,
+	Button,
+	Container,
+	Typography,
+	Paper,
+	Box,
+	Stack,
+} from "@mui/material";
+import bgRegister from "../assets/bgRegister.svg";
+import { useAuth } from "../components/Auth";
+import axios from "axios";
 
 const Register = () => {
-  const {login} = useAuth();
-  const [formData, setFormData] = useState({
-    nama: '',
-    username: '',
-    email: '',
-    password: '',
-    alamat: '',
-    kota: '',
-    no_hp: '',
-  });
+  const navigate = useNavigate();
+	const { login } = useAuth();
+	const [formData, setFormData] = useState({
+		nama: "",
+		username: "",
+		email: "",
+		password: "",
+		alamat: "",
+		kota: "",
+		no_hp: "",
+	});
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormData((prevData) => ({
+			...prevData,
+			[name]: value,
+		}));
+	};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    
-    if (!formData.username || !formData.email || !formData.password || !formData.alamat || !formData.kota || !formData.no_hp) {
-      alert('Please fill in all fields');
-      return;
-    }
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    try{
-      const response = await axios.post('http://localhost:3000/static/api/user/register', {
-        username,
-        email,
-        password,
-        alamat,
-        kota,
-        no_hp
-      });
-      console.log("a");
+		if (
+			!formData.username ||
+			!formData.email ||
+			!formData.password ||
+			!formData.alamat ||
+			!formData.kota ||
+			!formData.no_hp
+		) {
+			alert("Please fill in all fields");
+			return;
+		}
 
-      if (response.data && response.data.token) {
-        login(response.data);
+		try {
+			const response = await axios.post(
+				`${import.meta.env.VITE_API_URL}/api/user/register`,
+				{
+					nama: formData.nama,
+					username: formData.username,
+					email: formData.email,
+					password: formData.password,
+					alamat: formData.alamat,
+					kota: formData.kota,
+					no_hp: formData.no_hp,
+				},
+			);
 
-      } else {
-        console.error('Registration failed. Authentication data not received.');
-      }
+			setFormData({
+        nama : "",
+				username: "",
+				email: "",
+				password: "",
+				alamat: "",
+				kota: "",
+				no_hp: "",
+			});
 
-      setFormData({
-        username: '',
-        email: '',
-        password: '',
-        alamat: '',
-        kota: '',
-        no_hp: '',
-      });
-    } catch (error) {
-      console.error('Registration failed:', error.response.data.message);
-    }
+      navigate("/login");
+		} catch (error) {
+			console.error(error);
+			alert("Registration failed. Please try again.");
+		}
+	};
 
-    console.log('Registration form submitted:', formData);
-  };
-
-  return (
-    <>
-      {/* <Navbar /> */}
-      <div
-        style={{
-          backgroundImage: `url(${bgRegister})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          height: '100vh',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Container component="main" maxWidth="xs" style={{ marginTop: 30 }}>
-          <Paper elevation={3} style={{ padding: 16, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <Typography variant="h5">Register</Typography>
-            <form onSubmit={handleSubmit} style={{ width: '100%' }}>
-              <TextField
-                label="Nama"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                type="nama"
-                name="nama"
-                value={formData.nama}
-                onChange={handleChange}
-                required />
-              <TextField
-                label="Username"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                name="username"
-                value={formData.username}
-                onChange={handleChange}
-                required />
-              <TextField
-                label="Email"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required />
-              <TextField
-                label="Password"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                type="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                required />
-              <TextField
-                label="Alamat"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                type="alamat"
-                name="alamat"
-                value={formData.alamat}
-                onChange={handleChange}
-                required />
-              <TextField
-                label="Kota"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                type="kota"
-                name="kota"
-                value={formData.kota}
-                onChange={handleChange}
-                required />
-              <TextField
-                label="Nomor Telepon"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                type="no_hp"
-                name="no_hp"
-                value={formData.no_hp}
-                onChange={handleChange}
-                required />
-              <Button type="submit" variant="contained" color="primary" fullWidth style={{ marginTop: 16 }}>
-                Register
-              </Button>
-            </form>
-          </Paper>
-          <Typography variant="body2" style={{ marginTop: 10, textAlign: 'right' }}>
-            Sudah punya akun?{' '}
-            <NavLink component={NavLink} to="/login" style={{ textDecoration: 'none', color: 'blue' }}>
-              Login
-            </NavLink>
-          </Typography>
-        </Container>
-      </div>
-    </>
-  );
+	return (
+		<>
+			{/* <Navbar /> */}
+			<div
+				style={{
+					backgroundImage: `url(${bgRegister})`,
+					backgroundSize: "cover",
+					backgroundPosition: "center",
+					height: "100%",
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+			>
+				<Container component="main">
+					<Paper sx={{ mt: 5, mb: 5, mx: 30, py: 3 }} elevation={3}>
+						<Typography variant="h4" sx={{ textAlign: "center" }}>
+							Register
+						</Typography>
+						<br />
+						<Box component="form" onSubmit={handleSubmit}>
+							<Stack spacing={3} sx={{ alignItems: "center" }}>
+								<Stack direction="row" spacing={3}>
+									<TextField
+										type="text"
+										label="Nama"
+										variant="outlined"
+										name="nama"
+										value={formData.nama}
+										onChange={handleChange}
+										sx={{ width: 300 }}
+									/>
+									<TextField
+										type="text"
+										label="Username"
+										variant="outlined"
+										name="username"
+										value={formData.username}
+										onChange={handleChange}
+										sx={{ width: 300 }}
+									/>
+								</Stack>
+								<Stack direction="row" spacing={3}>
+									<TextField
+										type="email"
+										label="Email"
+										variant="outlined"
+										name="email"
+										value={formData.email}
+										onChange={handleChange}
+										sx={{ width: 300 }}
+									/>
+									<TextField
+										type="password"
+										label="Password"
+										variant="outlined"
+										name="password"
+										value={formData.password}
+										onChange={handleChange}
+										sx={{ width: 300 }}
+									/>
+								</Stack>
+								<TextField
+									type="text"
+									label="Alamat"
+									variant="outlined"
+									name="alamat"
+									value={formData.alamat}
+									onChange={handleChange}
+									sx={{ width: 625 }}
+								/>
+								<TextField
+									type="text"
+									label="Kota"
+									variant="outlined"
+									name="kota"
+									value={formData.kota}
+									onChange={handleChange}
+									sx={{ width: 625 }}
+								/>
+								<TextField
+									type="tel"
+									label="Nomor Telepon"
+									variant="outlined"
+									name="no_hp"
+									value={formData.no_hp}
+									onChange={handleChange}
+									sx={{ width: 625 }}
+								/>
+								<Button
+									type="submit"
+									variant="contained"
+									color="primary"
+									sx={{ width: 300 }}
+								>
+									Register
+								</Button>
+							</Stack>
+						</Box>
+						<Typography
+							variant="body1"
+							style={{ textAlign: "right", marginTop: 30, marginRight: 30 }}
+						>
+							Sudah punya akun?{" "}
+							<NavLink
+								component={NavLink}
+								to="/login"
+								style={{ textDecoration: "none", color: "blue" }}
+							>
+								Login
+							</NavLink>
+						</Typography>
+					</Paper>
+				</Container>
+			</div>
+		</>
+	);
 };
 
 export default Register;
